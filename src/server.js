5edc03edf53d6bdae3548756ed50f8c333a4a69d
alimukhtor <james1999007@gmail.com>
2021-12-15 14:44:58 +0100

    random

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import sequelize, { testDB } from './db/sequalization.js'
+const server = express()
+const port = process.env.PORT
+
+server.use(express.json())
+// server.use("/products", productsRouter)
+// server.use("/reviews", reviewsRouter)
+
+
+server.listen(port, async()=> {
+    console.log(`Server is running on port ${port}`);
+    await testDB()
+    await sequelize.sync();
+} )
