a8c385a8f22d12ee94ee3237bb507a562317116f
alimukhtor <james1999007@gmail.com>
2021-12-15 15:08:47 +0100

    created product table

---
+++ src/db/models/product.js
@@ -0,0 +1,46 @@
+import sequelize from "../sequalization.js";
+import s from "sequelize"
+const {DataTypes} = s
+// "id": 1,
+// "name": STRING,
+// "category": STRING, 
+// "description": TEXT, 
+// "image":"url(IMAGE LINK)",
+// "price":FLOAT,
+// "createdAt": "DATE",
+// "updatedAt": "DATE",   
+
+const Product = sequelize.define(
+    "product",
+    {
+        id:{
+            primaryKey:true,
+            type:DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        category:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        description:{
+            type:DataTypes.TEXT,
+            allowNull:false,
+        },
+        image:{
+            type:DataTypes.TEXT,
+            allowNull:false,
+        },
+        price:{
+            type:DataTypes.INTEGER,
+            allowNull:false
+        }
+
+    }
+
+);
+
+export default Product
