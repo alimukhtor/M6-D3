0819775e6e6fca21f8dff2d7d29020f01ed094bc
alimukhtor <james1999007@gmail.com>
2021-12-15 17:55:30 +0100

    passed productid as FK

--- src/server.js
+++ src/server.js
@@ -3,6 +3,17 @@ import sequelize, { testDB } from './db/sequalization.js'
 import productsRouter from './services/products/product.js'
 import reviewsRouter from './services/products/reviews/review.js'
 const server = express()
+// import Product from './product.js'
+import Product from './db/models/product.js'
+import Review from './db/models/review.js';
+
+
+Product.hasMany(Review);
+Review.belongsTo(Product);
+
+export {Product, Review};
+
+
 const port = process.env.PORT
 
 server.use(express.json())
