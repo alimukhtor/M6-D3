3e501ef197f1201f7602a52bad0e5534fa39e013
alimukhtor <james1999007@gmail.com>
2021-12-15 16:56:26 +0100

    passing product id as FK

--- src/services/products/reviews/review.js
+++ src/services/products/reviews/review.js
@@ -3,5 +3,27 @@ import Review from '../../../db/models/review.js'
 
 const reviewsRouter = express.Router()
 
+reviewsRouter.get("/", async(request, response, next)=> {
+    try {
+        const reviews = await Review.findAll({
+            attributes: {...request.body}
+        })
+
+        response.send(reviews)
+    } catch (error) {
+        next(error)
+    }
+})
+reviewsRouter.post("/", async(request, response, next)=> {
+    try {
+        const newReview = await Review.create(request.body)
+        response.status(201).send(newReview)        
+    } catch (error) {
+        next(error)
+    }
+})
+reviewsRouter.get("/:id", async(request, response, next)=> {})
+reviewsRouter.put("/:id", async(request, response, next)=> {})
+reviewsRouter.delete("/:id", async(request, response, next)=> {})
 
 export default reviewsRouter
