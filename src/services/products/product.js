47755018f58494d43b6d6bd165ce6bf76d2cab62
alimukhtor <james1999007@gmail.com>
2021-12-17 14:42:23 +0100

    created pagination

--- src/services/products/product.js
+++ src/services/products/product.js
@@ -1,127 +1,130 @@
-import express from 'express'
-import {Product, Review, Category, productCategory} from '../.././server.js'
-import {Op} from 'sequelize';
+import express from "express";
+import { Product, Review, Category, productCategory } from "../.././server.js";
+import { Op } from "sequelize";
 
+const productsRouter = express.Router();
 
-const productsRouter = express.Router()
+productsRouter.get("/", async (request, response, next) => {
+  try {
+    const products = await Product.findAll({
+      include: [Review, Category],
 
-productsRouter.get("/", async(request, response, next)=> {
-    try {
-        const products = await Product.findAll({
-            include:[Review, Category],
-           
-            where: {
-                ...(request.query.search && {
-                    [Op.or]:[
-                       {
-                        name: {[Op.iLike]: `%${request.query.search}%`},
-                       },
-                       {
-                        description: {[Op.iLike]: `%${request.query.search}%`},
-                       } 
-                    ]
-                })
-                
+      where: {
+        ...(request.query.search && {
+          [Op.or]: [
+            {
+              name: { [Op.iLike]: `%${request.query.search}%` },
             },
-            order:[['name', 'ASC']] 
-        })
-        response.send(products)
-    } catch (error) {
-        console.log("errro is:", error);
-        next(error)
-    }
-
-})
-
-productsRouter.get("/", async(request, response, next)=> {
-    try {
-        const products = await Product.findAll({
-            include: [
-                { 
-                model: Category,
-                through: { attributes: []},
-                where: {
-                    ...(request.query.category && {
-                      // if there is category in the filter
-                      name: {
-                        [Op.in]: request.query.category, // operator to filter by multiple category ( &category=Sql,React.js)
-                      },
-                    }),
-                  },
-                },
-        ],
-            order:[['name', 'ASC']] 
-        })
-        response.send(products)
-    } catch (error) {
-        console.log("errro is:", error);
-        next(error)
-    }
-
-})
-productsRouter.post("/", async(request, response, next)=> {
-    try {
-        console.log(request.body);
-        // const newProducts = await Product.create(request.body)
-        const {categoryId, ...restCategory} = request.body
-        const newProducts = await Product.create(restCategory)
-        if(newProducts){
-            console.log("Product id:", newProducts.id);
-            
-            const dataToInsert = categoryId.map((id) => ({
-                categoryId: id,
-                productId: newProducts.id,
-            }));
-            const data = await productCategory.bulkCreate(dataToInsert);
-            response.send({ newProducts, productCategory: data });
-        }   
-        response.status(201).send(newProducts)
-    } catch (error) {
-        console.log(error);
-        next(error)
-    }
-
-})
-productsRouter.get("/:id", async(request, response, next)=> {
-    try {
-        const getProductById = await Product.findByPk(request.params.id);
-        if(getProductById){
-            response.send(getProductById)
-        }else{
-            response.status(404).send(`The prduct with an id of ${request.params.id} is not found`)
-        }
-    } catch (error) {
-        next(error)
-    }
-})
-productsRouter.put("/:id", async(request, response, next)=> {
-    try {
-        const updateProduct = await Product.update(request.body, {
-            where:{id: request.params.id},
-            returning:true,
-        });
-
-        response.send(updateProduct[1][0])
-    } catch (error) {
-        next(error)
-    }
-
-})
-productsRouter.delete("/:id", async(request, response, next)=> {
-    try {
-        const deleteProduct = await Product.destroy({
-            where: {
-                id: request.params.id,
+            {
+              description: { [Op.iLike]: `%${request.query.search}%` },
             },
-        });        
-        if(deleteProduct>0){
-            response.send(`The chosen Product with a specific Id ${request.params.id} is deleted`)
-        }
-    } catch (error) {
-        next(error)
-    }
-})
+          ],
+        }),
+      },
+      order: [["name", "ASC"]],
+      limit:request.query.limit,
+      offset: parseInt(request.query.limit * request.query.page)
+    });
+    response.send(products);
+  } catch (error) {
+    console.log("errro is:", error);
+    next(error);
+  }
+});
 
+productsRouter.get("/", async (request, response, next) => {
+  try {
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          through: { attributes: [] },
+          attributes: ["name"],
+        },
+      ],
+      where: {
+        ...(request.query.search && {
+          [Op.or]: [
+            {
+              name: { [Op.iLike]: `%${request.query.search}%` },
+            },
+            {
+              description: { [Op.iLike]: `%${request.query.search}%` },
+            },
+          ],
+        }),
+      },
+      order: [["name", "ASC"]],
+    });
+    response.send(products);
+  } catch (error) {
+    console.log("errro is:", error);
+    next(error);
+  }
+});
+productsRouter.post("/", async (request, response, next) => {
+  try {
+    console.log(request.body);
+    // const newProducts = await Product.create(request.body)
+    const { categoryId, ...restCategory } = request.body;
+    const newProducts = await Product.create(restCategory);
+    if (newProducts) {
+      console.log("Product id:", newProducts.id);
+
+      const dataToInsert = categoryId.map((id) => ({
+        categoryId: id,
+        productId: newProducts.id,
+      }));
+      const data = await productCategory.bulkCreate(dataToInsert);
+      response.send({ newProducts, productCategory: data });
+    }
+    response.status(201).send(newProducts);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+productsRouter.get("/:id", async (request, response, next) => {
+  try {
+    const getProductById = await Product.findByPk(request.params.id);
+    if (getProductById) {
+      response.send(getProductById);
+    } else {
+      response
+        .status(404)
+        .send(`The prduct with an id of ${request.params.id} is not found`);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+productsRouter.put("/:id", async (request, response, next) => {
+  try {
+    const updateProduct = await Product.update(request.body, {
+      where: { id: request.params.id },
+      returning: true,
+    });
+
+    response.send(updateProduct[1][0]);
+  } catch (error) {
+    next(error);
+  }
+});
+productsRouter.delete("/:id", async (request, response, next) => {
+  try {
+    const deleteProduct = await Product.destroy({
+      where: {
+        id: request.params.id,
+      },
+    });
+    if (deleteProduct > 0) {
+      response.send(
+        `The chosen Product with a specific Id ${request.params.id} is deleted`
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default productsRouter;
-
